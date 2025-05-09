9511dd27d09b7fa40357ca160673a88f26972332
Geovane Girard <g.ng2009@hotmail.com>
2025-05-09 09:53:03 -0300

    routes refactoring

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as http from "http";
 import {getFilterEpisodes, getlistEpisodes} from './controllers/podcasts-controller';
+import { Routes } from "./routes/routes";
 
 
 
@@ -7,11 +8,11 @@ const server = http.createServer( async(req: http.IncomingMessage, res: http.Ser
 
     const [baseUrl, queryString] = req.url?.split("?") ?? [", "];
 
-    if (req.method === "GET" && baseUrl === "/api/list") {
+    if (req.method === "GET" && baseUrl === Routes.LIST) {
         await getlistEpisodes(req, res);
     }
 
-    if (req.method === "GET" && baseUrl === "/api/episode") {
+    if (req.method === "GET" && baseUrl === Routes.EPISODE) {
         await getFilterEpisodes(req, res);
     }
 
